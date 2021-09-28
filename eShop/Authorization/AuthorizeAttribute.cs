ab4589dbcdfbe30ffce358ddbb2ed44daf017e2b
helenaJovanovic <41912173+helenaJovanovic@users.noreply.github.com>
2021-09-28 16:38:53 +0200

    feat: Addcustom authorization attributes and entities

---
+++ eShop/Authorization/AuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace eShop.Authorization
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute: Attribute, IAuthorizationFilter
+    {
+        private readonly IList<Role> _roles;
+
+        public AuthorizeAttribute(params Role[] roles)
+
+        {
+            _roles = roles ?? new Role[] { };
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //check if action is decorated with allowanonymous if yes, then skip
+            //that means the action is going to get executed
+            var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+            if (allowAnonymous)
+            {
+                return;
+            }
+
+            var user = (User)context.HttpContext.Items["User"];
+            if (user == null || (_roles.Any() && !_roles.Contains(user.Role)))
+            {
+                context.Result = new JsonResult(new { message = "Unathorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
